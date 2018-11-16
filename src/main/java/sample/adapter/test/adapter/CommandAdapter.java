2950cabd30545140f17117bd45c0f8e260c9b7e7
wadakc <kei-wada@mail.nissan.co.jp>
2018-11-16 12:56:27 +0900

    repo

---
+++ src/main/java/sample/adapter/test/adapter/CommandAdapter.java
@@ -0,0 +1,13 @@
+package sample.adapter.test.adapter;
+
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CommandAdapter {
+
+
+    public String getUser(String accessToken){
+        return "get User kc0520 2!!!!!";
+    }
+}
